f9ceab83b31d237380783f67d9022c96aba61cba
wzb <2482924813@qq.com>
2024-12-17 11:58:18 +0800

    Stream exercise 2024.12.17

--- JavaExercise2024.12.10/src/exercise/Sale.java
+++ JavaExercise2024.12.10/src/exercise/Sale.java
@@ -10,10 +10,6 @@ public class Sale {
         Thread thread2 = new Thread(saleTickets, "窗口2");
         Thread thread3 = new Thread(saleTickets, "窗口3");
         Thread thread4 = new Thread(saleTickets, "窗口4");
-        thread1.setPriority(10);
-        thread2.setPriority(9);
-        thread3.setPriority(8);
-        thread4.setPriority(7);
         thread1.start();
         thread2.start();
         thread3.start();
--- JavaExercise2024.12.10/src/pojo/SaleTickets.java
+++ JavaExercise2024.12.10/src/pojo/SaleTickets.java
@@ -1,47 +1,46 @@
-// SaleTickets.java
 package pojo;
-
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class SaleTickets implements Runnable {
-    // 使用 AtomicInteger 保证原子操作
-    private AtomicInteger tickets = new AtomicInteger(100);
-    // 使用 ConcurrentHashMap 存储每个窗口卖出的票数
-    private ConcurrentHashMap<String, Integer> soldTickets = new ConcurrentHashMap<>();
-    // 标志位，用于通知线程停止，使用 AtomicBoolean 保证原子性和可见性
-    private AtomicBoolean soldOut = new AtomicBoolean(false);
-
+    private int tickets = 100;
+    private final Object lock = new Object();
 
     @Override
     public void run() {
-        String windowName = Thread.currentThread().getName();
-        // 初始化每个窗口卖出票数为 0
-        soldTickets.put(windowName, 0);
-        while (!soldOut.get()) {
-            int remainingTickets;
-            int sold = soldTickets.get(windowName);
-            do {
-                remainingTickets = tickets.get();
-                if (remainingTickets <= 0) {
-                    soldOut.set(true);
-                    break;
-                }
-            } while (!tickets.compareAndSet(remainingTickets, remainingTickets - 1));
-            if (remainingTickets > 0) {
-                try {
-                    // 模拟出售时间
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    e.printStackTrace();
-                }
-                soldTickets.put(windowName, sold + 1);
-                System.out.println(windowName + "正在出售第" + (sold + 1) + "张票，剩余" + (remainingTickets - 1) + "张票");
+        while (true) {
+//            synchronized (lock) {
+//                if (tickets > 0) {
+//                    System.out.println(Thread.currentThread().getName() + "正在卖第" + tickets + "张票");
+//                    try {
+//                        Thread.sleep(10);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    tickets--;
+//                }
+//            }
+//            if (tickets == 0) {
+//                break;
+//            }
+
+            sale();
+            if (tickets == 0) {
+                System.out.println("票卖完了");
+                break;
             }
         }
-        System.out.println(windowName + " 共卖出 " + soldTickets.get(windowName) + " 张票");
+    }
+
+    private synchronized void sale() {
+        if (tickets > 0) {
+            System.out.println(Thread.currentThread().getName() + "正在卖第" + tickets + "张票");
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        tickets--;
+        }
+
     }
 }
